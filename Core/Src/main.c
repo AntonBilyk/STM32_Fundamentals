2f98d22fbef62beacd512f8409d23a8e25384917
Toxi4 <98952218+AntonBilyk@users.noreply.github.com>
2026-08-19 12:41:33 +0300

    LED Blinking (The same interval)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -92,13 +92,13 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+  while(1)
   {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-    HAL_Delay(1000);
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
