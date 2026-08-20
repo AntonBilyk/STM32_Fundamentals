a5f79d4511293c223499214f0132379760066ae1
Toxi4 <98952218+AntonBilyk@users.noreply.github.com>
2026-08-20 13:57:10 +0300

    Add delay with different of/on duration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -97,8 +97,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-	  HAL_Delay(1000);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	  HAL_Delay(500);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	  HAL_Delay(2000);
   }
   /* USER CODE END 3 */
 }
